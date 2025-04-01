d75aa67b672958c02cac285170df10b832b5b340
Arya Nagvekar <157247888+Red2Ninja@users.noreply.github.com>
2025-04-02 01:40:40 +0530

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Header";
-import Note from "./Note";
 import Footer from "./Footer";
+import Details from "./member";
+import Note from "./Note";
+import Secretary from "./Secretary";  
 
-function App(){
-    return( <div>
-        <Header />
-        <Note />
-        <Footer />
 
-    </div>);
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Note />} />
+        <Route path="/member" element={<Details />} />
+        <Route path="/Secretary" element={<Secretary />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App;
+export default App;
