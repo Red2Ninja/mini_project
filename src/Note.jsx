16a5dfcbc376b103287995be8dd6202308c44f68
Arya Nagvekar <157247888+Red2Ninja@users.noreply.github.com>
2025-04-02 01:41:20 +0530

    Update Note.jsx

--- src/Note.jsx
+++ src/Note.jsx
@@ -1,12 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 function Note() {
+  const navigate = useNavigate();
+
   return (
     <div className="note">
       <h1>Automate Club Attendance</h1>
       <p>Track attendance and night slips seamlessly.</p>
-      <button className="btn primary">Members</button>
-      <button className="btn secondary">Board</button>
+      <button className="btn primary" onClick={() => navigate("/member")}>Members</button>
+      <button className="btn secondary" onClick={() => navigate("/Secretary")}>Board</button>
+
     </div>
   );
 }
 
-export default Note;
+export default Note;
