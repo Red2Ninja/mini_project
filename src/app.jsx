57155aaa0a148b737adce67a210660f65a7f9d49
Mrinali Charhate <163443152+mrinaliii@users.noreply.github.com>
2025-04-02 19:00:11 +0530

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,11 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Details from "./member";
 import Note from "./Note";
-import Secretary from "./Secretary";  
+import Secretary from "./Secretary";
+import MemberForm from "./memberform";  
+import BoardForm from "./BoardForm";
+import SecretaryDashboard from "./SecretaryDashboard";
+import MemberDashboard from "./MemberDashboard";
 
 
 function App() {
@@ -12,12 +16,18 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Note />} />
-        <Route path="/member" element={<Details />} />
+        <Route path="/memberform" element={<MemberForm />} />
         <Route path="/Secretary" element={<Secretary />} />
+        <Route path="/memberform" element={<MemberForm />} /> 
+        <Route path="/BoardForm" element={<BoardForm />} /> 
+        <Route path="/SecretaryDashboard" element={<SecretaryDashboard />} />
+        <Route path = "/MemberDashboard" element={<MemberDashboard />} />
+        <Route path = "/member" element={<Details />} />
+
       </Routes>
       <Footer />
     </Router>
   );
 }
 
-export default App;
+export default App;
